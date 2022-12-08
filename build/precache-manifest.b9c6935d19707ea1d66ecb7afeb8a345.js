dd898fad9bb31b3857d013beda979c9fc9fc141a
Ravi Verma <ravivermahd123@gmail.com>
2022-12-08 17:15:16 +0530

    Initial commit

---
+++ build/precache-manifest.b9c6935d19707ea1d66ecb7afeb8a345.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "751bfc20afdf9c22ab8d",
+    "url": "/react-gh-pages/static/css/main.ef87c019.chunk.css"
+  },
+  {
+    "revision": "751bfc20afdf9c22ab8d",
+    "url": "/react-gh-pages/static/js/main.78bc3895.chunk.js"
+  },
+  {
+    "revision": "06bc68505dd4d2fe4fa4",
+    "url": "/react-gh-pages/static/js/runtime~main.f0c3f962.js"
+  },
+  {
+    "revision": "31890b3ac6c1b40a917a",
+    "url": "/react-gh-pages/static/css/2.104ecded.chunk.css"
+  },
+  {
+    "revision": "31890b3ac6c1b40a917a",
+    "url": "/react-gh-pages/static/js/2.e2c1550f.chunk.js"
+  },
+  {
+    "revision": "1ad0af76033d25b423aa",
+    "url": "/react-gh-pages/static/js/3.11315e87.chunk.js"
+  },
+  {
+    "revision": "1909cf4f362ac7c107d9b9e315ba3e7d",
+    "url": "/react-gh-pages/static/media/carousel-1.1909cf4f.jpg"
+  },
+  {
+    "revision": "1766d96fd33537db24068b1194928ea8",
+    "url": "/react-gh-pages/static/media/about.1766d96f.jpg"
+  },
+  {
+    "revision": "f54e7375fca1448521635b4adafc8dec",
+    "url": "/react-gh-pages/static/media/team-1.f54e7375.jpg"
+  },
+  {
+    "revision": "24cd4aaf7be68c36b2d8d41c7517b9c6",
+    "url": "/react-gh-pages/static/media/team-2.24cd4aaf.jpg"
+  },
+  {
+    "revision": "312f9e435f2c7d379fb9eece5070b72a",
+    "url": "/react-gh-pages/static/media/team-3.312f9e43.jpg"
+  },
+  {
+    "revision": "48677843da7982bb74100be092751e13",
+    "url": "/react-gh-pages/static/media/team-4.48677843.jpg"
+  },
+  {
+    "revision": "7f21752ca70c16b3b7e9726586c6a59f",
+    "url": "/react-gh-pages/static/media/price.7f21752c.jpg"
+  },
+  {
+    "revision": "5ad614b15a46005b7d7a0110a3e73fa2",
+    "url": "/react-gh-pages/static/media/open.5ad614b1.jpg"
+  },
+  {
+    "revision": "c1105651c26fb77c6615d096a58998dd",
+    "url": "/react-gh-pages/static/media/carousel-2.c1105651.jpg"
+  },
+  {
+    "revision": "592643a83b8541edc52063d84c468700",
+    "url": "/react-gh-pages/static/media/fa-brands-400.592643a8.eot"
+  },
+  {
+    "revision": "ed311c7a0ade9a75bb3ebf5a7670f31d",
+    "url": "/react-gh-pages/static/media/fa-brands-400.ed311c7a.woff2"
+  },
+  {
+    "revision": "1a575a4138e5f366474f0e7c5bd614a5",
+    "url": "/react-gh-pages/static/media/fa-brands-400.1a575a41.woff"
+  },
+  {
+    "revision": "513aa607d398efaccc559916c3431403",
+    "url": "/react-gh-pages/static/media/fa-brands-400.513aa607.ttf"
+  },
+  {
+    "revision": "b0e2db3b634d1bc3928e127458d993d8",
+    "url": "/react-gh-pages/static/media/fa-regular-400.b0e2db3b.eot"
+  },
+  {
+    "revision": "b91d376b8d7646d671cd820950d5f7f1",
+    "url": "/react-gh-pages/static/media/fa-regular-400.b91d376b.woff2"
+  },
+  {
+    "revision": "d1d7e3b4c219fde0f7376c6facfd7149",
+    "url": "/react-gh-pages/static/media/fa-regular-400.d1d7e3b4.woff"
+  },
+  {
+    "revision": "766913e6c0088ab8c9f73e18b4127bc4",
+    "url": "/react-gh-pages/static/media/fa-regular-400.766913e6.ttf"
+  },
+  {
+    "revision": "c5d109be8edd3de0f60eb472bd9ef691",
+    "url": "/react-gh-pages/static/media/fa-regular-400.c5d109be.svg"
+  },
+  {
+    "revision": "0c6bfc668a72935760178f91327aed3a",
+    "url": "/react-gh-pages/static/media/fa-solid-900.0c6bfc66.eot"
+  },
+  {
+    "revision": "d824df7eb2e268626a2dd9a6a741ac4e",
+    "url": "/react-gh-pages/static/media/fa-solid-900.d824df7e.woff2"
+  },
+  {
+    "revision": "d745348d289b149026921f197929a893",
+    "url": "/react-gh-pages/static/media/fa-solid-900.d745348d.woff"
+  },
+  {
+    "revision": "b9625119ce4300f0ef890a8f3234c773",
+    "url": "/react-gh-pages/static/media/fa-solid-900.b9625119.ttf"
+  },
+  {
+    "revision": "1d5619cd804367cefe6da2d79289218a",
+    "url": "/react-gh-pages/static/media/fa-brands-400.1d5619cd.svg"
+  },
+  {
+    "revision": "37bc7099f6f1ba80236164f22e905837",
+    "url": "/react-gh-pages/static/media/fa-solid-900.37bc7099.svg"
+  },
+  {
+    "revision": "1a0ad7eb3287219c4bb9dbaa162b9f7d",
+    "url": "/react-gh-pages/index.html"
+  }
+];
